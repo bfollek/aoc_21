d0bc62c5d6b845ea2a11faa2b5b5f8680b2adb10
Bob Follek <bfollek@gmail.com>
2021-12-01 08:42:54 -0500

    Tweaks ATP

--- src/day_01/day_01.rb
+++ src/day_01/day_01.rb
@@ -2,17 +2,24 @@ class Day01
   # "How many measurements are larger than the previous measurement?"
   def part_1(file_name)
     lines = File.readlines(file_name, chomp: true).map &:to_i
-    lines
-      .filter.with_index { |nxt, i| i > 0 && nxt > lines[i - 1] }
-      .size
+    count_larger lines
   end
 
   # Consider sums of a three-measurement sliding window. How many sums are larger than the previous sum?
   def part_2(file_name)
     lines = File.readlines(file_name, chomp: true).map &:to_i
     windows = lines.each_cons(3).to_a.map &:sum
-    windows
-      .filter.with_index { |nxt, i| i > 0 && nxt > windows[i - 1] }
+    count_larger windows
+  end
+
+  # -----------------------------------------------------------------
+  private
+
+  # -----------------------------------------------------------------
+
+  def count_larger(ary)
+    ary
+      .filter.with_index { |nxt, i| i > 0 && nxt > ary[i - 1] }
       .size
   end
 end
