e6761b67ff0634463e1622bcc68be9aed4115c13
Bob Follek <bfollek@gmail.com>
2021-12-01 06:22:21 -0500

    day 1 part 1 done

--- src/day_01/day_01.rb
+++ src/day_01/day_01.rb
@@ -1,4 +1,15 @@
 class Day01
-
-    def part_1(file_name)
+  # "How many measurements are larger than the previous measurement?"
+  def part_1(file_name)
+    prev = nil
+    cnt = 0
+    lines = File.readlines(file_name, chomp: true).map &:to_i
+    lines.each do |line|
+      if prev && line > prev
+        cnt += 1
+      end
+      prev = line
     end
+    cnt
+  end
+end
