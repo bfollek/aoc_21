090b07cafede0e6fee0ef885776e5ccfb4b8e90d
Bob Follek <bfollek@gmail.com>
2021-12-01 06:22:30 -0500

    WIP

---
+++ README.md
@@ -0,0 +1,21 @@
+# aoc_21
+
+Advent of Code 2021 in ruby.
+
+## Running Tests
+
+Run one day:
+
+```bash
+bundle exec ruby test/test_day_01.rb
+```
+
+Run all tests:
+
+```bash
+rake
+```
+
+## Days
+
+### Day 1
---
+++ test/test_day_01.rb
@@ -0,0 +1,12 @@
+require "minitest/autorun"
+
+require_relative "../src/day_01/day_01"
+
+class Day01Test < Minitest::Test
+  def test_part_1
+    dir = File.dirname(__FILE__)
+    file_name = File.join(dir, "../data/day_01.txt")
+    day_01 = Day01.new
+    assert_equal 1475, day_01.part_1(file_name)
+  end
+end
