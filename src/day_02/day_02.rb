c1a6acf3a86beb873023a8fbce02647c74572e6f
Bob Follek <bfollek@gmail.com>
2021-12-05 17:19:12 -0500

    Day 2 part 1 working. Time to refactor.

--- src/day_02/day_02.rb
+++ src/day_02/day_02.rb
@@ -1,7 +1,18 @@
 class Day02
   def part_1(file_name)
-    moves = File.readlines(file_name, chomp: true).map &:to_i
-    nil
+    pos = Position.new
+    moves = Move.load_file(file_name)
+    moves.each do |move|
+      case move.direction
+      when "forward"
+        pos.horizontal += move.distance
+      when "up"
+        pos.depth -= move.distance
+      when "down"
+        pos.depth += move.distance
+      end
+    end
+    pos.depth * pos.horizontal
   end
 
   def part_2(file_name)
--- src/day_02/move.rb
+++ src/day_02/move.rb
@@ -1,9 +1,22 @@
 class Move
-  def new
+  attr_reader :direction, :distance
+
+  def initialize(direction, distance)
+    @direction = direction
+    @distance = distance
   end
 
   def self.load_file(file_name)
+    moves = []
     lines = File.readlines(file_name, chomp: true)
-    #raw.
+    lines.each do |line|
+      m = line.match /(forward|up|down) (\d+)/
+      if m
+        moves << self.new(m[1], m[2].to_i)
+      else
+        raise "Unknown move: #{line}"
+      end
+    end
+    moves
   end
 end
