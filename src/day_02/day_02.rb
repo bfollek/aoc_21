1fc7ab3e1e2ff9a156fb84e0618a9b9f18c4f52e
Bob Follek <bfollek@gmail.com>
2021-12-05 16:45:10 -0500

    Working on day 2. ruby-fied the data and doc file names.

---
+++ src/day_02/day_02.rb
@@ -0,0 +1,13 @@
+class Day02
+  def part_1(file_name)
+    moves = File.readlines(file_name, chomp: true).map &:to_i
+    nil
+  end
+
+  def part_2(file_name)
+    nil
+  end
+
+  # -----------------------------------------------------------------
+  private
+end
---
+++ src/day_02/move.rb
@@ -0,0 +1,9 @@
+class Move
+  def new
+  end
+
+  def self.load_file(file_name)
+    lines = File.readlines(file_name, chomp: true)
+    #raw.
+  end
+end
