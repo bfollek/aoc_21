2408625b9582e2b2e34c2b43386c19bd258140f4
Bob Follek <bfollek@gmail.com>
2021-12-05 17:27:06 -0500

    Position.change handles move details

--- src/day_02/day_02.rb
+++ src/day_02/day_02.rb
@@ -5,17 +5,8 @@ class Day02
   def part_1(file_name)
     pos = Position.new
     moves = Move.load_file(file_name)
-    moves.each do |move|
-      case move.direction
-      when "forward"
-        pos.horizontal += move.distance
-      when "up"
-        pos.depth -= move.distance
-      when "down"
-        pos.depth += move.distance
-      end
-    end
-    pos.depth * pos.horizontal
+    moves.each { |move| pos.change move }
+    return pos.depth * pos.horizontal
   end
 
   def part_2(file_name)
--- src/day_02/position.rb
+++ src/day_02/position.rb
@@ -1,8 +1,19 @@
 class Position
-  attr_accessor :depth, :horizontal
+  attr_reader :depth, :horizontal
 
   def initialize
     @depth = 0
     @horizontal = 0
+  end
+
+  def change(move)
+    case move.direction
+    when "forward"
+      @horizontal += move.distance
+    when "up"
+      @depth -= move.distance
+    when "down"
+      @depth += move.distance
+    end
   end
 end
