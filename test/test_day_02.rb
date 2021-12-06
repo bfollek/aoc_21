fe70cffcaa5b091f633edef79a6ccbceb20595f9
Bob Follek <bfollek@gmail.com>
2021-12-05 22:14:05 -0500

    Added day 2 test for unknown move exception

--- test/test_day_02.rb
+++ test/test_day_02.rb
@@ -16,4 +16,14 @@ class Day02Test < Minitest::Test
     day_02 = Day02.new
     assert_equal 1975421260, day_02.part_2(file_name)
   end
+
+  def test_unknown_move
+    dir = File.dirname(__FILE__)
+    file_name = File.join(dir, "../data/day_02_unknown_move.txt")
+    day_02 = Day02.new
+    ex = assert_raises(RuntimeError) do
+      assert_equal -1, day_02.part_2(file_name)
+    end
+    assert_match(/unknown move/i, ex.message)
+  end
 end
