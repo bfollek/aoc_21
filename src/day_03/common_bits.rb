1b9629ce1813e5ce5865ac42d5dce1fc96c0f158
Bob Follek <bfollek@gmail.com>
2021-12-06 13:36:38 -0500

    Tweaked

--- src/day_03/common_bits.rb
+++ src/day_03/common_bits.rb
@@ -8,11 +8,13 @@ class CommonBits
 
   def count(diagnostics)
     counters = [0] * diagnostics[0].size
+
     diagnostics.each do |d|
       d.each_char.with_index do |c, i|
         counters[i] += c.to_i
       end
     end
+
     counters.each do |cnt|
       m, l = cnt >= (diagnostics.size / 2) ? ["1", "0"] : ["0", "1"]
       @most_common << m
