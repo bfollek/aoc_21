331e82f1d35d9a900a6651a292a9717364034717
Bob Follek <bfollek@gmail.com>
2021-12-07 19:15:48 -0500

    Refactoring ATP

--- src/day_03/day_03.rb
+++ src/day_03/day_03.rb
@@ -1,12 +1,10 @@
-require_relative "./common_bits"
-
 MOST_COMMON = 1
 LEAST_COMMON = 0
 
 class Day03
   def part_1(file_name)
     diagnostics = File.readlines(file_name, chomp: true)
-    most_common, least_common = CommonBits.count(diagnostics)
+    most_common, least_common = count_common_bits(diagnostics)
     gamma = most_common.join.to_i(2)
     epsilon = least_common.join.to_i(2)
     gamma * epsilon
@@ -22,12 +20,31 @@ class Day03
   # -----------------------------------------------------------------
   private
 
+  def count_common_bits(diagnostics)
+    counters = [0] * diagnostics[0].size
+
+    diagnostics.each do |d|
+      d.each_char.with_index do |c, i|
+        counters[i] += c.to_i
+      end
+    end
+
+    most_common = []
+    least_common = []
+    counters.each do |cnt|
+      m, l = cnt >= (diagnostics.size / 2.0) ? ["1", "0"] : ["0", "1"]
+      most_common << m
+      least_common << l
+    end
+    [most_common, least_common]
+  end
+
   def filter_rating(diagnostics, most_common_or_least)
     (0...diagnostics[0].size).each do |i|
       if diagnostics.size <= 1
         break
       end
-      most_common, least_common = CommonBits.count(diagnostics)
+      most_common, least_common = count_common_bits(diagnostics)
       filter_bits = most_common_or_least == MOST_COMMON ? most_common : least_common
       diagnostics = diagnostics.filter { |d| d[i] == filter_bits[i] }
     end
--- test/test_day_03.rb
+++ test/test_day_03.rb
@@ -7,14 +7,14 @@ class Day03Test < Minitest::Test
     dir = File.dirname(__FILE__)
     file_name = File.join(dir, "../data/day_03_short.txt")
     diagnostics = File.readlines(file_name, chomp: true)
-    most_common, least_common = CommonBits.count(diagnostics)
+    most_common, least_common = Day03.new.send("count_common_bits", diagnostics)
     assert_equal ["1", "0", "1", "1", "0"], most_common
     assert_equal ["0", "1", "0", "0", "1"], least_common
   end
 
   def test_most_common_second_bit
     diagnostics = ["11110", "10110", "10111", "10101", "11100", "10000", "11001"]
-    most_common, least_common = CommonBits.count(diagnostics)
+    most_common, least_common = Day03.new.send("count_common_bits", diagnostics)
     assert_equal "0", most_common[1]
   end
 
