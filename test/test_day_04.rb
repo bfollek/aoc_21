3701dafd6188fa2c383d4b47e7c66a5b483bdad9
Bob Follek <bfollek@gmail.com>
2021-12-09 17:27:37 -0500

    ATP

--- test/test_day_04.rb
+++ test/test_day_04.rb
@@ -11,7 +11,6 @@ class Day04Test < Minitest::Test
   end
 
   def test_part_1
-    skip
     dir = File.dirname(__FILE__)
     file_name = File.join(dir, "../data/day_04.txt")
     day_04 = Day04.new
