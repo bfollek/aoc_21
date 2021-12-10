45b8ff188be18632d431563a94bca16b075c1e28
Bob Follek <bfollek@gmail.com>
2021-12-09 19:13:54 -0500

    Making tests easier to clone for each new day

--- test/test_day_05.rb
+++ test/test_day_05.rb
@@ -3,31 +3,34 @@ require "minitest/autorun"
 require_relative "../src/day_05/day_05"
 
 class Day05Test < Minitest::Test
+  SHORT_FILE = "../data/day_05_short.txt"
+  LONG_FILE = "../data/day_05.txt"
+
   def test_part_1_short
     dir = File.dirname(__FILE__)
-    file_name = File.join(dir, "../data/day_05_short.txt")
-    day_05 = Day05.new
-    assert_equal -1, day_05.part_1(file_name)
+    file_name = File.join(dir, SHORT_FILE)
+    day = Day05.new
+    assert_equal -1, day.part_1(file_name)
   end
 
   def test_part_1
     dir = File.dirname(__FILE__)
-    file_name = File.join(dir, "../data/day_05.txt")
-    day_05 = Day05.new
-    assert_equal -1, day_05.part_1(file_name)
+    file_name = File.join(dir, LONG_FILE)
+    day = Day05.new
+    assert_equal -1, day.part_1(file_name)
   end
 
   def test_part_2_short
     dir = File.dirname(__FILE__)
-    file_name = File.join(dir, "../data/day_05_short.txt")
-    day_05 = Day05.new
-    assert_equal -1, day_05.part_2(file_name)
+    file_name = File.join(dir, SHORT_FILE)
+    day = Day05.new
+    assert_equal -1, day.part_2(file_name)
   end
 
   def test_part_2
     dir = File.dirname(__FILE__)
-    file_name = File.join(dir, "../data/day_05.txt")
-    day_05 = Day05.new
-    assert_equal -1, day_05.part_2(file_name)
+    file_name = File.join(dir, LONG_FILE)
+    day = Day05.new
+    assert_equal -1, day.part_2(file_name)
   end
 end
