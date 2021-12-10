e3e64f324cb17f63ec17173b44d014451864cf14
Bob Follek <bfollek@gmail.com>
2021-12-09 19:22:43 -0500

    WIP

--- test/test_day_05.rb
+++ test/test_day_05.rb
@@ -3,34 +3,38 @@ require "minitest/autorun"
 require_relative "../src/day_05/day_05"
 
 class Day05Test < Minitest::Test
-  SHORT_FILE = "../data/day_05_short.txt"
-  LONG_FILE = "../data/day_05.txt"
+  DAY = "05"
+
+  SHORT_FILE = "../data/day_#{DAY}_short.txt"
+  LONG_FILE = "../data/day_#{DAY}.txt"
+
+  CLASS = Object.const_get("Day#{DAY}") # Assumes class exists
 
   def test_part_1_short
     dir = File.dirname(__FILE__)
     file_name = File.join(dir, SHORT_FILE)
-    day = Day05.new
+    day = CLASS.new
     assert_equal -1, day.part_1(file_name)
   end
 
   def test_part_1
     dir = File.dirname(__FILE__)
     file_name = File.join(dir, LONG_FILE)
-    day = Day05.new
+    day = CLASS.new
     assert_equal -1, day.part_1(file_name)
   end
 
   def test_part_2_short
     dir = File.dirname(__FILE__)
     file_name = File.join(dir, SHORT_FILE)
-    day = Day05.new
+    day = CLASS.new
     assert_equal -1, day.part_2(file_name)
   end
 
   def test_part_2
     dir = File.dirname(__FILE__)
     file_name = File.join(dir, LONG_FILE)
-    day = Day05.new
+    day = CLASS.new
     assert_equal -1, day.part_2(file_name)
   end
 end
