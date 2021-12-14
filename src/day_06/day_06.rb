da6dcf2f9619192945614886ea3df0a06579cebb
Bob Follek <bfollek@gmail.com>
2021-12-13 23:38:13 -0500

    Day 6 done ATP

--- src/day_06/day_06.rb
+++ src/day_06/day_06.rb
@@ -1,10 +1,11 @@
 class Day06
   NUM_SLOTS = 9
+  PREGNANT_AGAIN = 6
 
   # "How many lanternfish would there be after 80 days?"
-  def part_1(file_name, days)
-    population = File.read(file_name, chomp: true).split(",").map &:to_i
-    population_by_due_date = simulate_fast population, days
+  def part_1(file_name, days_to_run)
+    population_by_fish = File.read(file_name, chomp: true).split(",").map &:to_i
+    population_by_due_date = simulate population_by_fish, days_to_run
     population_by_due_date.sum
   end
 
@@ -15,22 +16,22 @@ class Day06
   # -----------------------------------------------------------------
   private
 
-  def simulate_fast(population, days)
+  def simulate(population_by_fish, days_to_run)
     # "Each day, a 0 becomes a 6 and adds a new 8 to the end of the list, while each other number decreases by 1 if it was present at the start of the day."
     # So a fish can have a due date of 0 to 8 days. Use `population_by_due_date` to count the number of fish with each due date.
     population_by_due_date = [0] * NUM_SLOTS
-    population.each do |fish|
+    population_by_fish.each do |fish|
       population_by_due_date[fish] += 1
     end
-    #debug "starting population_by_due_date: #{population_by_due_date}"
-    days.times do
-      tmp = population_by_due_date[0]
+    #debug "Starting population_by_due_date: #{population_by_due_date}"
+    days_to_run.times do
       # Shift each slot down
+      births = population_by_due_date[0]
       (0..NUM_SLOTS - 2).each do |i|
         population_by_due_date[i] = population_by_due_date[i + 1]
       end
-      population_by_due_date[6] += tmp # Births
-      population_by_due_date[NUM_SLOTS - 1] = tmp
+      population_by_due_date[NUM_SLOTS - 1] = births
+      population_by_due_date[PREGNANT_AGAIN] += births
     end
     population_by_due_date
   end
