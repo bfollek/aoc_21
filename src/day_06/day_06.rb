d4e1efc6553611ffce91b8dc224bd5d195920d28
Bob Follek <bfollek@gmail.com>
2021-12-13 21:32:44 -0500

    Starting day 6

---
+++ src/day_06/day_06.rb
@@ -0,0 +1,13 @@
+class Day06
+  # "How many lanternfish would there be after 80 days?"
+  def part_1(file_name)
+    -1
+  end
+
+  def part_2(file_name)
+    -1
+  end
+
+  # -----------------------------------------------------------------
+  private
+end
