b28941dad6c155869c51ed35b58e7ab595c94fd6
Bob Follek <bfollek@gmail.com>
2021-12-21 11:42:03 -0500

    Day 3: cleaner way to build counters in count_common_bits

--- src/day_03/day_03.rb
+++ src/day_03/day_03.rb
@@ -3,34 +3,34 @@ class Day03
   LEAST_COMMON = 0
 
   def part_1(file_name)
-    diagnostics = File.readlines(file_name, chomp: true)
+    diagnostics = load_diagnostics(file_name)
     most_common, least_common = count_common_bits(diagnostics)
-    gamma = most_common.join.to_i(2)
-    epsilon = least_common.join.to_i(2)
+    gamma = bit_array_to_number(most_common)
+    epsilon = bit_array_to_number(least_common)
     gamma * epsilon
   end
 
   def part_2(file_name)
-    diagnostics = File.readlines(file_name, chomp: true)
-    oxygen = filter_rating(diagnostics, MOST_COMMON).to_i(2)
-    co2 = filter_rating(diagnostics, LEAST_COMMON).to_i(2)
+    diagnostics = load_diagnostics(file_name)
+    oxygen = bit_array_to_number(filter_rating(diagnostics, MOST_COMMON))
+    co2 = bit_array_to_number(filter_rating(diagnostics, LEAST_COMMON))
     oxygen * co2
   end
 
   # -----------------------------------------------------------------
   private
 
+  def load_diagnostics(file_name)
+    lines = File.readlines(file_name, chomp: true)
+    lines.map { |line| line.split("").map &:to_i }
+  end
+
   def count_common_bits(diagnostics)
-    counters = [0] * diagnostics[0].size
-    diagnostics.each do |d|
-      d.each_char.with_index do |c, i|
-        counters[i] += c.to_i
-      end
-    end
+    counters = diagnostics.transpose.map &:sum
     most_common = []
     least_common = []
     counters.each do |cnt|
-      m, l = cnt >= (diagnostics.size / 2.0) ? ["1", "0"] : ["0", "1"]
+      m, l = cnt >= (diagnostics.size / 2.0) ? [1, 0] : [0, 1]
       most_common << m
       least_common << l
     end
@@ -47,5 +47,9 @@ class Day03
       diagnostics = diagnostics.filter { |d| d[i] == filter_bits[i] }
     end
     diagnostics[0]
+  end
+
+  def bit_array_to_number(bits)
+    bits.map { |bit| bit.to_s }.join.to_i(2)
   end
 end
--- test/test_day_03.rb
+++ test/test_day_03.rb
@@ -6,16 +6,16 @@ class Day03Test < Minitest::Test
   def test_common_bits_short
     dir = File.dirname(__FILE__)
     file_name = File.join(dir, "../data/day_03_short.txt")
-    diagnostics = File.readlines(file_name, chomp: true)
+    diagnostics = Day03.new.send("load_diagnostics", file_name)
     most_common, least_common = Day03.new.send("count_common_bits", diagnostics)
-    assert_equal ["1", "0", "1", "1", "0"], most_common
-    assert_equal ["0", "1", "0", "0", "1"], least_common
+    assert_equal [1, 0, 1, 1, 0], most_common
+    assert_equal [0, 1, 0, 0, 1], least_common
   end
 
   def test_most_common_second_bit
-    diagnostics = ["11110", "10110", "10111", "10101", "11100", "10000", "11001"]
+    diagnostics = [[1, 1, 1, 1, 0], [1, 0, 1, 1, 0], [1, 0, 1, 1, 1], [1, 0, 1, 0, 1], [1, 1, 1, 0, 0], [1, 0, 0, 0, 0], [1, 1, 0, 0, 1]]
     most_common, least_common = Day03.new.send("count_common_bits", diagnostics)
-    assert_equal "0", most_common[1]
+    assert_equal 0, most_common[1]
   end
 
   def test_part_1
